e4dd406180a0f13c5fb5f07cdac23fe10a90beab
Minhyuk Kim <rhrlwld@gmail.com>
2022-08-01 21:46:33 +0900

    1st

---
+++ client/src/components/Config.js
@@ -0,0 +1,6 @@
+//SERVER ROUTES
+export const USER_SERVER = '/api/users';
+
+export const API_URL = 'https://api.themoviedb.org/3/'
+export const API_KEY = '7ed96d4bbf2205cbfb8bebb76bd9f11b'
+export const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/'
